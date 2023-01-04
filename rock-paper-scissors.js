1ed56ad398c6993f6fd501669402ec3de5d5b1bf
David Pinkerton <david@dpinkerton.com>
2023-01-04 10:57:20 +1000

    Use returns and describe game outcomes.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -20,38 +20,59 @@ function getComputerChoice () {
     (randomBelowThree < 1) ? computerSelection = "rock" :
     (randomBelowThree < 2) ? computerSelection = "paper" :
     computerSelection = "scissors";
+    return(computerSelection)
 }
 
 function getPlayerChoice () {
     playerSelection = (prompt("Will you play rock, paper or scissors?", "rock")).toLowerCase();
+    return(playerSelection);
 }
 
-getComputerChoice();
+console.log("You chose " + getPlayerChoice() + ". The computer chose " + getComputerChoice() + ". " + playRound(playerSelection, computerSelection) + " " + describeGame (gameOutcome, computerSelection));
 
-getPlayerChoice();
-
-function playRound (playerSelection, computerSelection) {
-       
+function playRound (playerSelection, computerSelection) {  
+    
     if (computerSelection == playerSelection) {
         gameOutcome = "draw";
         return(`It's a ${gameOutcome}.`)
     }
     
     else if (computerSelection == "rock") {
-        (playerSelection == "paper") ? gameOutcome = "win" : gameOutcome = "lose";
+        (playerSelection == "paper") ?
+            gameOutcome = "win" :
+            gameOutcome = "lose";
+            return(`You ${gameOutcome}!`);
     }
     
     else if (computerSelection == "paper") {
-        (playerSelection == "rock") ? gameOutcome = "lose" : gameOutcome = "win";
-        return(`You ${gameOutcome}!`);
+        (playerSelection == "rock") ?
+            gameOutcome = "lose" :
+            gameOutcome = "win";
+            return(`You ${gameOutcome}!`);
 
     }
     else if (computerSelection == "scissors") {
-        (playerSelection == "rock") ? gameOutcome = "win" : gameOutcome = "lose";
-        return(`You ${gameOutcome}!`);
+        (playerSelection == "rock") ?
+            gameOutcome = "win" :
+            gameOutcome = "lose";
+            return(`You ${gameOutcome}!`);
 
     }
 }
 
-console.log(playRound(playerSelection, computerSelection)); 
+function describeGame (gameOutcome, computerSelection) {
+    
+    if ((gameOutcome == "win" && computerSelection == "rock") || (gameOutcome == "lose" && computerSelection == "paper")) {
+        return("Paper covers rock.");        
+    }        
+    else if ((gameOutcome == "win" && computerSelection == "paper") || (gameOutcome == "lose" && computerSelection == "scissors")) {
+        return("Scissors cut paper.");        
+    }
+    else if ((gameOutcome == "win" && computerSelection == "scissors") || (gameOutcome == "lose" && computerSelection == "rock")) {
+        return("Rock crushes scissors.");        
+    }
+    else {
+        return("You chose alike.");
+    }        
+}
 
