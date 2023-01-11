d5f3dc54417065608aaa7cf0accfb536bbabe57f
David Pinkerton <david@dpinkerton.com>
2023-01-11 13:23:23 +1000

    Get halfway to a button working

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,37 +6,37 @@ function getComputerChoice () {
     return(computerSelection)
 }
 
-function getPlayerChoice () {
-    playerSelection = (prompt("Will you play rock, paper or scissors?", "rock")).toLowerCase();
-    return(playerSelection);
-}
+//function getPlayerChoice () {
+//    playerSelection = (prompt("Will you play rock, paper or scissors?", "rock")).toLowerCase();
+//    return(playerSelection);
+//}
 
 function playRound (playerSelection, computerSelection) {  
     
     if (computerSelection == playerSelection) {
         gameOutcome = "draw";
-        return(`It's a ${gameOutcome}.`)
+        alert(`It's a ${gameOutcome}.`)
     }
     
     else if (computerSelection == "rock") {
         (playerSelection == "paper") ?
             gameOutcome = "win" :
             gameOutcome = "lose";
-            return(`You ${gameOutcome}!`);
+            alert(`You ${gameOutcome}!`);
     }
     
     else if (computerSelection == "paper") {
         (playerSelection == "rock") ?
             gameOutcome = "lose" :
             gameOutcome = "win";
-            return(`You ${gameOutcome}!`);
+            alert(`You ${gameOutcome}!`);
 
     }
     else if (computerSelection == "scissors") {
         (playerSelection == "rock") ?
             gameOutcome = "win" :
             gameOutcome = "lose";
-            return(`You ${gameOutcome}!`);
+            alert(`You ${gameOutcome}!`);
 
     }
 }
@@ -57,15 +57,38 @@ function describeGame (gameOutcome, computerSelection) {
     }        
 }
 
-for (let i = 0, winCount = 0, drawCount = 0; i < 5; i++) {   
+let playerSelection = "";
+const buttonRock = document.getElementById("rock");
+buttonRock.addEventListener("click", function() {
+    playerSelection = "rock";
+    console.log('rock clicked');
+    i = 0;
+    winCount = 0;
+    drawCount = 0;
 
-    let roundDescription = playRound(getPlayerChoice(), getComputerChoice());
+    let roundDescription = playRound(playerSelection, getComputerChoice());
 
     let choicesCompared = describeGame(gameOutcome, computerSelection);
 
     (gameOutcome == "win") ? winCount++ : null ;
     (gameOutcome == "draw") ? drawCount++ : null ;
+});
+
+
+//for (let i = 0, winCount = 0, drawCount = 0; i < 5; i++) {   
+
+//i = 0;
+//winCount = 0;
+//drawCount = 0;
+
+//let roundDescription = playRound(playerSelection, getComputerChoice());
+
+//let choicesCompared = describeGame(gameOutcome, computerSelection);
+
+//(gameOutcome == "win") ? winCount++ : null ;
+//(gameOutcome == "draw") ? drawCount++ : null ;
     
-    alert("You chose " + playerSelection + ". The computer chose " + computerSelection + ". " + roundDescription + " " + choicesCompared + " You've won " + winCount + " of " + (i +1) + " games and drawn " + drawCount + ".");
+//alert("You chose " + playerSelection + ". The computer chose " + computerSelection + ". " + roundDescription + " " + choicesCompared + " You've won " + winCount + " of " + (i +1) + " games and drawn " + drawCount + ".");
   
-}
+//}
+
